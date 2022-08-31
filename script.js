c13fcb3469b01778103fa0cfdeb75cef5eb2cef8
minkman <lomick2090@gmail.com>
2022-08-31 14:57:41 -0500

    Finish most calculator functionality

--- script.js
+++ script.js
@@ -1,14 +1,74 @@
 buttons = document.querySelectorAll('button');
-screen = document.querySelector(".screen");
-let input
-console.log(buttons); 
+answerbox = document.querySelector(".answerbox");
+input = document.querySelector(".input");
+var a = '';
+var b = '';
+let answer;
+let operatorInUse;
+
+function userInput(button) {
+    if (button.className === 'clear'){
+        a='';
+        b='';
+        input.textContent='';
+        answerbox.textContent='';
+        operatorInUse = '';
+        answer = '';
+    } else if (button.className === 'number') {
+        inputNumber(button.textContent);
+    } else if (button.className === 'operator') {
+        inputOperator(button.textContent);
+    }
+};
+
+function inputOperator(operator) {
+
+    if (a == '' || a == undefined){
+        if (input.textContent != '') {
+            a = parseInt(input.textContent);
+            input.textContent = '';
+            answerbox.textContent = `${a} ${operator}`;
+        }
+    } else if (input.textContent != '') {
+            b = parseInt(input.textContent);
+            a = doMath(a , b , operatorInUse);
+            b = '';
+            input.textContent = ''; 
+    } else {
+        answerbox.textContent = `${a} ${operator}`;
+    }
+    
+
+    operatorInUse = operator;
+
+
+};
+
+function doMath(a, b , operator) {
+    switch (operator){
+        case '+':
+            answer = a + b;
+            break;
+        case '*':
+            answer = a * b;
+            break;
+        case '/':
+            answer = a / b;
+            break;
+        case '-':
+            answer = a - b;
+            break;
+    }
+    if (answer % 1 != 0 ) answer = answer.toFixed(5);
+    answerbox.textContent = answer;
+    return answer;
+};
 
 function inputNumber(num) {
-    let text = screen.textContent + num;
-    screen.textContent = text;
-    input = text;
-}
+    let text = input.textContent + num;
+    input.textContent = text;
+};
 
 buttons.forEach ((button) => 
-    button.addEventListener('click', () => inputNumber(button.textContent)
+    button.addEventListener('click', () => userInput(button)
     ));
